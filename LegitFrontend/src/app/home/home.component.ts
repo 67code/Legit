51b33219252a512c65ce0732001eb293786ee432
syedabdul <syedabdul@propelinc.com>
2025-02-18 20:03:38 +0530

    commit for frontend base setup

--- LegitFrontend/src/app/home/home.component.ts
+++ LegitFrontend/src/app/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  mission = "Not Every Store Is Legit—But We’ll Tell You Which Ones Are!";
+  mission: string = "Empowering Trust, One Business at a Time.";
 }
